15002cc01f5ab3be7367a5d8a44f00f52d84ca57
Gabriel Shannon <gabrielshannon96@gmail.com>
2022-08-11 17:49:54 +0100

    routing structure

---
+++ src/components/MovieInformation/MoveInformation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function MoveInformation() {
+  console.log('movie info');
+  return (
+    <div>MoveInformation</div>
+  );
+}
+
+export default MoveInformation;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Profile() {
+  console.log('profile');
+  return (
+    <div>Profile</div>
+  );
+}
+
+export default Profile;
